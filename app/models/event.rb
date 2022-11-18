ddccbe64cdd876c2a74fcc7b2161d1ed9afdd6ce
chrismenke45 <chrismenke45@gmail.com>
2022-11-17 21:01:03 -0800

    Event views in progress

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 4..40 }
+  validates :description, presence: true, length: { maximum: 300 }
+  validates :date, presence: true
 end
