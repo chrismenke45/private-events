f52ab6a3cff3c473a07322ef8475e8e3fd7b125d
chrismenke45 <chrismenke45@gmail.com>
2022-11-22 08:56:53 -0800

    added dep destroy and scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,12 @@
 class Event < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, length: { in: 4..40 }
   validates :description, presence: true, length: { maximum: 300 }
-  validates :date, presence: true
+  validates :date, presence: true, comparison: { greater_than: DateTime.now, message: "must be set in the future" }
 
   belongs_to :host, class_name: "User"
-  has_many :enrollments, foreign_key: :attended_event_id
+  has_many :enrollments, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :enrollments
+
+  scope :past, -> { where("date < ?", DateTime.now).order(date: :desc) }
+  scope :upcoming, -> { where("date >= ?", DateTime.now).order(date: :asc) }
 end
