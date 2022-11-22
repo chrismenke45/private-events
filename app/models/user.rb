a7b22756fd7f93466c83ade5ab8d22740ed7a251
chrismenke45 <chrismenke45@gmail.com>
2022-11-22 08:56:24 -0800

    added dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true, length: { in: 1..35 }
   validates :family_name, presence: true, length: { in: 1..35 }
 
-  has_many :hosted_events, class_name: "Event", foreign_key: "host_id", inverse_of: "host"
-  has_many :enrollments, foreign_key: :attendee_id
+  has_many :hosted_events, class_name: "Event", foreign_key: "host_id", inverse_of: "host", dependent: :destroy
+  has_many :enrollments, foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, through: :enrollments
 end
