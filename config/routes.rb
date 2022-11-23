8a97825923b438498a8db601e5716018db52dbcb
chrismenke45 <chrismenke45@gmail.com>
2022-11-23 11:52:17 -0800

    added users#show

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
                        sessions: "users/sessions",
                        registrations: "users/registrations",
                      }
+  get "/users/:id", to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
